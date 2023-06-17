dce99fda9717842ee776a79a63f72a7e3d32fef1
Midriaz <scome@inbox.ru>
2023-06-17 19:46:21 +0700

    mvp

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 from functools import wraps
 import openai
 from flask_migrate import Migrate
-# from models import Conversation, Message
 
 
 load_dotenv()
@@ -27,9 +27,14 @@ openai.api_key = OPENAI_API_KEY
 
 
 class Conversation(db.Model):
+    __tablename__ = "conversation"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
     messages = db.relationship('Message', backref='conversation', lazy=True)
+
+    # This is the display name for the model when queried
+    def __repr__(self):
+        return f"{self.name}"
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,8 +45,8 @@ class Message(db.Model):
 def limit_ip_access(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        if request.remote_addr not in WHITELISTED_IPS:
-            abort(403)  # Forbidden
+        # if request.remote_addr not in WHITELISTED_IPS:
+        #     abort(403)  # Forbidden
         return func(*args, **kwargs)
     return decorated
 
@@ -49,9 +54,6 @@ def limit_ip_access(func):
 def get_response(conversation, prompt):
     # Format the messages for the OpenAI API
     messages = [{"role": message.role, "content": message.content} for message in conversation.messages]
-
-    # Add the system message at the beginning
-    messages.insert(0, {"role": "system", "content": "You are a helpful assistant."})
     messages.append({"role": "user", "content": prompt})
 
     response = openai.ChatCompletion.create(
@@ -61,36 +63,49 @@ def get_response(conversation, prompt):
     return response['choices'][0]['message']['content']
 
 
+def render_home(id = None, new = False):
+    # order by the newest message
+    conversations = db.session.query(Conversation)\
+        .join(Message, Conversation.id == Message.conversation_id)\
+        .order_by(desc(Message.id)).all()
+
+    if id:
+        conversation = Conversation.query.get(id)
+    elif len(conversations) > 0 and not new:
+        conversation = conversations[0]
+    else:
+        # Create a new Conversation object
+        conversation = Conversation(title="Untitled Conversation")
+        db.session.add(conversation)
+        db.session.commit()
+
+        # Add the system message at the beginning
+        message = Message(role='system', content="You are a helpful assistant.", conversation_id=conversation.id)
+        db.session.add(message)
+        # Save the changes to the database
+        db.session.commit()
+    return render_template('home.html', conversations=conversations, conversation=conversation) 
+
+
 @app.route('/')
 @limit_ip_access
 def main_menu():
-    conversations = Conversation.query.all()
-    return render_template('home.html', conversations=conversations)
+    return render_home()
 
 @app.route('/chat/<int:id>')
 @limit_ip_access
 def view_chat(id):
-    conversation = Conversation.query.get(id)
-    return render_template('home.html', conversation=conversation)
+    return render_home(id=id)
 
-@app.route('/chat/new', methods=['POST'])
+@app.route('/chat/new', methods=['GET'])
 @limit_ip_access
 def new_chat():
-    # Create a new Conversation object
-    conversation = Conversation(title="Untitled Conversation")
-    db.session.add(conversation)
-
-    # Save the changes to the database
-    db.session.commit()
-
-    # Redirect to the chat view for the new conversation
-    return redirect(url_for('view_chat', id=conversation.id))
+    return render_home(new=True)
 
 
 @app.route('/chat/<int:id>/regenerate', methods=['POST'])
 @limit_ip_access
 def regenerate_response(id):
-    # Get the conversation
     conversation = Conversation.query.get(id)
 
     # Get the last assistant's message and delete it
@@ -101,7 +116,7 @@ def regenerate_response(id):
     last_user_message = Message.query.filter_by(conversation_id=id, role='user').order_by(Message.id.desc()).first()
 
     # Generate a new response using the OpenAI API
-    new_response = get_response(last_user_message.content)
+    new_response = get_response(conversation, last_user_message.content)
 
     # Create a new Message object for the assistant's response
     new_assistant_message = Message(role="assistant", content=new_response, conversation_id=id)
@@ -135,8 +150,28 @@ def send_message(id):
     assistant_message = Message(role="assistant", content=response, conversation_id=id)
     db.session.add(assistant_message)
 
-    # Save the changes to the database
+     # Save the changes to the database
     db.session.commit()
+
+    # If this is the first user message, update the conversation title
+    if len(conversation.messages) == 3:
+        messages = [
+            {"role": "user", "content": user_message.content}, 
+            {"role": "assistant", "content": assistant_message.content},
+            {"role": "user", "content": "Name our conversation (max 50 chars)"} 
+        ]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        title = response['choices'][0]['message']['content']
+        title = title.replace('"', '')
+        conversation.title = title[:50]
+        db.session.commit()
 
     # Redirect back to the chat view
     return redirect(url_for('view_chat', id=id))
+
+@app.route('/myip', methods=['get'])
+def show_ip():
+    return request.remote_addr
